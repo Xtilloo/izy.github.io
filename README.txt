508841625521eb6230e4e5a29b05cbcf9d797534
BostonLarson <larson.boston@outlook.com>
2025-11-05 22:10:14 -0700

    Cleaned up repo

--- README.txt
+++
@@ -1,2 +0,0 @@
-*This project is based on the learning resources: "Build an HTML5 website with a responsive layout."
-This is a learning project, to gain experience building a site with HTML5.
--- js/app.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById('newsletter-form');
-const submitEmail = document.getElementById('user-email-button');
-const userEmail = document.getElementById('user-email').value;
-
-submitEmail.addEventListener("click", function() {
-    form.submit();
-    alert(`email received: ${userEmail}`)
-});
